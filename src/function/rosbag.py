b66082b4716b0fdd84bd3a714b21072d820101a6
bomenakim <96710675+Bomena@users.noreply.github.com>
2024-03-03 23:15:18 +0900

    Update rosbag.py

--- src/function/rosbag.py
+++ src/function/rosbag.py
@@ -10,13 +10,11 @@ import datetime
 path = "/home/dataset"
 rosbag_process = None
 
-# Rosbag 녹음 상태 확인
 def check_rosbag_status():
     global path
     list_of_files = glob.glob(path + '/*.bag.active')
-    return "yes" if list_of_files else "no"
+    return True if list_of_files else False
 
-# Rosbag 파일 크기 확인
 def get_rosbag_size():
     global path
     list_of_files = glob.glob(path + '/*.bag.active')
@@ -25,21 +23,33 @@ def get_rosbag_size():
 
     latest_file = max(list_of_files, key=os.path.getctime)
     size = os.path.getsize(latest_file)
+    size_gb = size / (1024 * 1024 * 1024)
+    return f"{size_gb:.3f} GB"
 
-    # Bytes를 MB로 변환
-    size_mb = size / (1024 * 1024) * 0.001
-    return f"{size_mb:.3f} GB"
+def rosbag_record(data):
+    global rosbag_process
+    if data.data == "ON":
+        if rosbag_process is not None:
+            subprocess.call(["pkill", "-f", "rosbag record"])
+            rosbag_process = None
+        rosbag_process = subprocess.Popen(["/home/Web/sensor_monitoring/record.sh"])
+    elif data.data == "OFF":
+        if rosbag_process:
+            subprocess.call(["pkill", "-f", "rosbag record"])
+            rosbag_process = None
+
+def listener():
+    rospy.Subscriber('/rosbag_record', String, rosbag_record)
 
 def main():
-    rospy.init_node('rosbag_control_node')
+    rospy.init_node('rosbag_control_node', anonymous=True)
+    listener()
 
-    # 서비스, 퍼블리셔 설정
-    status_pub = rospy.Publisher('/rosbag_status', String, queue_size=10)
+    status_pub = rospy.Publisher('/rosbag_status', Bool, queue_size=10)
     size_pub = rospy.Publisher('/rosbag_size', String, queue_size=10)
 
-    rate = rospy.Rate(0.05)  # 1 Hz
+    rate = rospy.Rate(1)
     while not rospy.is_shutdown():
-        # Rosbag 상태 및 크기 정보 전송
         status = check_rosbag_status()
         size = get_rosbag_size()
         status_pub.publish(status)
